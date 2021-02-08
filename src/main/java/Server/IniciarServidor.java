d6adb989e86210f72d04eb1eebe5e7e516827b9b
Borja <borjamoremart@gmail.com>
2021-02-08 20:29:37 +0100

    Esqueleto0.1V

---
+++ src/main/java/Server/IniciarServidor.java
@@ -0,0 +1,17 @@
+package Server;
+
+public class IniciarServidor {
+
+
+    public static void main(String[] args) {
+        IniciarServidor servidor = new IniciarServidor();
+        servidor.iniciar();
+    }
+
+
+    //Aqui generamos un bucle de escucha infinito que lanzara un hilo para gestionar cada peticion
+    private void iniciar(){
+
+    }
+
+}
