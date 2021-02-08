fc60fd0cca7985f6346f1a2f66c053301009d9e7
Borja <borjamoremart@gmail.com>
2021-02-08 20:40:17 +0100

    Esqueleto0.1V

--- src/main/java/Entity/Jugador.java
+++ src/main/java/Entity/Jugador.java
@@ -2,4 +2,13 @@ package Entity;
 
 public class Jugador {
 
+    private String nickname;
+
+    private String juego;
+
+    private String ip;
+
+    private String puerto;
+
+
 }
--- src/main/java/Entity/Partida.java
+++ src/main/java/Entity/Partida.java
@@ -1,4 +1,13 @@
 package Entity;
 
 public class Partida {
+
+    private int id;
+
+    private String tipo;
+
+    //Si pueden entrar jugadores o no
+    private boolean abierta;
+
+
 }
