8d251f81700be3de87dc9ece85dda17fe73cf08c
Borja <borjamoremart@gmail.com>
2021-02-08 20:28:26 +0100

    Esqueleto0.1V

---
+++ src/main/java/Server/BuscarPartida.java
@@ -0,0 +1,32 @@
+package Server;
+
+import Entity.Jugador;
+
+/**
+ * Aqui vemos el tipo de partida que quiere el jugador, buscamos si hay alguna creada a la espera o
+ * creamos una a la espera
+ */
+public class BuscarPartida {
+
+    //Buscamos en la lista del tipo de juego que quiere el jugador a ver si hay alguna disponible
+    private void buscarEnLista(){
+        //si no hay cramos una nuevaPartida();
+        //Si hay obtenemos los datos de la partida unirmePartida();
+    }
+
+    private void nuevaPartida(){
+        //Cramos la partida y la añadimos a la lista y esperamos a que alquien se una
+        //si se une alguien lanzamos la partida
+    }
+    private void lanzarPartida(Jugador jugadorqueseune){
+        //Lanzamos la partida diciendole al jugador que el va ser el host y quien se va unir
+        //cerramos la conxion
+    }
+
+    private void unirmePartida(){
+        //Enviar los datos del jugador al que se tiene que unir
+        //cerramos la conexion
+    }
+
+
+}
---
+++ src/main/java/Server/ProcesarJugador.java
@@ -0,0 +1,34 @@
+package Server;
+
+import Entity.Jugador;
+
+/**
+ * Aqui hay que gestionar si el clienteesta enviando el cierre de una partida o quiere jugar una nueva
+ */
+public class ProcesarJugador implements Runnable{
+
+    //Hilo de conexion con el cliente
+    @Override
+    public void run() {
+
+    }
+    //Analizamos si la peticion es para cerrar un juego o buscar una nueva partida
+    private void leerPeticion{
+
+    }
+
+    //Buscamos el juego y lo cerramos de la lista
+    private void cerrarJuego( /*La identificación de la partida*/ ){
+
+    }
+    //Generamos un Jugador nuevo y lo mandamos al emparejador de partidas--> "BuscarPartida" Class
+    private void nuevaPartida(){
+
+    }
+    //Crear un Jugador
+    private Jugador nuevoJugador(){
+        Jugador j = new Jugador();
+        return j;
+    }
+
+}
